b69101f57bd56eefe77af1d543a153d8c28449b9
jmarias-edu <jmarias@up.edu.ph>
2024-05-07 00:39:22 +0800

    feat: added vuetify to frontend

--- omgenes/frontend/src/App.vue
+++ omgenes/frontend/src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About this application</router-link>
-  </nav>
-  <router-view/>
+    <v-app>
+    <v-navigation-drawer>
+      <v-list-item title="OMGenes LMAO"></v-list-item>
+      <v-divider></v-divider>
+      <router-link to="/"><v-list-item title="Home"></v-list-item></router-link>
+      <router-link to="/about"><v-list-item link title="About"></v-list-item></router-link>
+    </v-navigation-drawer>
+    <v-app-bar :elevation="5">
+      <v-app-bar-title>OMGenes</v-app-bar-title>
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <GoogleLogin :callback="callback"/>
+      </template>
+    </v-app-bar>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script setup> 
+const callback = (response) => {
+  console.log("Handle the response", response)
 }
+</script>
 
-nav {
-  padding: 30px;
+// <style lang="scss">
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+// nav {
+//   padding: 30px;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
+// </style>
--- omgenes/frontend/src/main.js
+++ omgenes/frontend/src/main.js
@@ -3,5 +3,21 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vue3GoogleLogin from 'vue3-google-login';
 
-createApp(App).use(store).use(router).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App)
+.use(store)
+.use(router)
+.use(vuetify)
+.use(vue3GoogleLogin, {clientId: "129850221361-koj4k3re7jjhr33i42ok2f3pj5f28mu2.apps.googleusercontent.com"})
+.mount('#app')
