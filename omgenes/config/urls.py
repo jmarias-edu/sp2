08f60a15e0d5854d22830a794f828b3f7b9708a0
jmarias-edu <jmarias@up.edu.ph>
2024-05-11 10:54:54 +0800

    feat: removed original user login

--- omgenes/config/urls.py
+++ omgenes/config/urls.py
@@ -11,64 +11,64 @@ from drf_spectacular.views import SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
 
+# urlpatterns = [
+#     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+#     path(
+#         "about/",
+#         TemplateView.as_view(template_name="pages/about.html"),
+#         name="about",
+#     ),
+#     # Django Admin, use {% url 'admin:index' %}
+#     path(settings.ADMIN_URL, admin.site.urls),
+#     # User management
+#     path("users/", include("omgenes.users.urls", namespace="users")),
+#     path("accounts/", include("allauth.urls")),
+#     # Your stuff: custom urls includes go here
+#     # ...
+#     # Media files
+#     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+# ]
+
+# # API URLS
+# urlpatterns += [
+#     # API base url
+#     path("api/", include("config.api_router")),
+#     # DRF auth token
+#     path("api/auth-token/", obtain_auth_token),
+#     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+#     path(
+#         "api/docs/",
+#         SpectacularSwaggerView.as_view(url_name="api-schema"),
+#         name="api-docs",
+#     ),
+# ]
+
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/",
-        TemplateView.as_view(template_name="pages/about.html"),
-        name="about",
-    ),
-    # Django Admin, use {% url 'admin:index' %}
-    path(settings.ADMIN_URL, admin.site.urls),
-    # User management
-    path("users/", include("omgenes.users.urls", namespace="users")),
-    path("accounts/", include("allauth.urls")),
-    # Your stuff: custom urls includes go here
-    # ...
-    # Media files
-    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-]
-
-# API URLS
-urlpatterns += [
-    # API base url
-    path("api/", include("config.api_router")),
-    # DRF auth token
-    path("api/auth-token/", obtain_auth_token),
-    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
-    path(
-        "api/docs/",
-        SpectacularSwaggerView.as_view(url_name="api-schema"),
-        name="api-docs",
-    ),
-]
-
-urlpatterns += [
     path("google/", include("gauth.urls"))
 ]
 
-if settings.DEBUG:
-    # This allows the error pages to be debugged during development, just visit
-    # these url in browser to see how these error pages look like.
-    urlpatterns += [
-        path(
-            "400/",
-            default_views.bad_request,
-            kwargs={"exception": Exception("Bad Request!")},
-        ),
-        path(
-            "403/",
-            default_views.permission_denied,
-            kwargs={"exception": Exception("Permission Denied")},
-        ),
-        path(
-            "404/",
-            default_views.page_not_found,
-            kwargs={"exception": Exception("Page not Found")},
-        ),
-        path("500/", default_views.server_error),
-    ]
-    if "debug_toolbar" in settings.INSTALLED_APPS:
-        import debug_toolbar
+# if settings.DEBUG:
+#     # This allows the error pages to be debugged during development, just visit
+#     # these url in browser to see how these error pages look like.
+#     urlpatterns += [
+#         path(
+#             "400/",
+#             default_views.bad_request,
+#             kwargs={"exception": Exception("Bad Request!")},
+#         ),
+#         path(
+#             "403/",
+#             default_views.permission_denied,
+#             kwargs={"exception": Exception("Permission Denied")},
+#         ),
+#         path(
+#             "404/",
+#             default_views.page_not_found,
+#             kwargs={"exception": Exception("Page not Found")},
+#         ),
+#         path("500/", default_views.server_error),
+#     ]
+#     if "debug_toolbar" in settings.INSTALLED_APPS:
+#         import debug_toolbar
 
-        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+#         urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
